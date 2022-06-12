0d2f581389017d27278339f01afc7c757ed5ff80
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2022-06-12 16:33:22 +0200

    update

--- js/printcv.js
+++ js/printcv.js
@@ -53,3 +53,31 @@ function printCVShort() {
   }
   return true;
 }
+
+function printCVReallyShort() {
+  var w = window.open('', 'PRINTCV', 'width=1064');
+  w.document.write(
+    '<head>'+
+    '<meta charset="utf-8">'+
+    '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'+
+    '<link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">'+
+    '<title></title>'+
+    '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">'+
+    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>'+
+    '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>'+
+    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>'+
+    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>'+
+    '<link href="/cv.css" rel="stylesheet">'+
+    '</head>'+
+    '<body>'+
+    '<main role="main" class="inner cover">'
+  );
+  w.document.write(document.getElementById('cv').innerHTML.replaceAll('<details', '<details hidden = "true" ').replaceAll('<div class="ReallySimple" hidden="true">', '<div class="ReallySimple">'));
+  w.document.write('</main></body></html>');
+  w.document.close(); // necessary for IE >= 10
+  w.focus(); // necessary for IE >= 10
+  w.onload = () => {
+    w.print();
+  }
+  return true;
+}
